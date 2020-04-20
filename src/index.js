747f5e02365f0ee5be641602bf690628c8e71876
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2020-04-20 08:13:25 -0400

    polyfillvalues

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import './fonts/CircularStd-Black.otf'
-
+if (!Object.values) Object.values = o=>Object.keys(o).map(k=>o[k]);
 
 // import '@fortawesome/fontawesome-free/css/all.min.css'; 
 // import'bootstrap-css-only/css/bootstrap.min.css'; 
