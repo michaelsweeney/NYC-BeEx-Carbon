ebbdb1f6f718068948c20db2e4ac6d90043e812c
Michael Sweeney <michaelsweeney@Sweeney-MacBook-Air.local>
2021-06-08 10:29:24 -0400

    update nyc url

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Footer = props => {
-	return (
-		<div className="footer">
-			<div className="footer-left">
-				<p>
-					What now? Visit{' '}
-					<a href="https://retrofitaccelerator.cityofnewyork.us/" target="_blank" rel="noopener noreferrer">
-						NYC Accelerator
-					</a>{' '}
-					for free, personalized advisory services to improve building energy efficiency and lower carbon
-					emissions.
-				</p>
-			</div>
-			<div className="footer-right">
-				<p>
-					<a href="http://www.akfgroup.com" target="_blank" rel="noopener noreferrer">
-						Calculator engine by AKF Group LLC
-					</a>
-				</p>
-			</div>
-		</div>
-	);
+const Footer = (props) => {
+  const retrofiturl =
+    "https://www1.nyc.gov/site/nycaccelerator/index.page?utm_source=BEEx&utm_medium=LL97_Calc&utm_campaign=Evergreen";
+  const akfurl = "http://www.akfgroup.com";
+
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <p>
+          What now? Visit{" "}
+          <a href={retrofiturl} target="_blank" rel="noopener noreferrer">
+            NYC Accelerator
+          </a>{" "}
+          for free, personalized advisory services to improve building energy
+          efficiency and lower carbon emissions.
+        </p>
+      </div>
+      <div className="footer-right">
+        <p>
+          <a href={akfurl} target="_blank" rel="noopener noreferrer">
+            Calculator engine by AKF Group LLC
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export { Footer };
